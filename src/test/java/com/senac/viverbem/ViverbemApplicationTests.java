7418f624315b273d28d7413183975149d6c6b4be
Raul <raulbtmonteiro@gmail.com>
2024-07-18 21:00:13 -0300

    WIP

--- src/test/java/com/senac/viverbem/ViverbemApplicationTests.java
+++ src/test/java/com/senac/viverbem/ViverbemApplicationTests.java
@@ -1,13 +1,12 @@
 package com.senac.viverbem;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource(properties = {"DATABASE_URL=${env.DATABASE_URL}"})
 class ViverbemApplicationTests {
 
-
+    @Test
+    void contextLoads() {
+    }
 }
